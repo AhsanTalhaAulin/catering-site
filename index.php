9abf4de17049c879fc3f7b33209bd28ce4b900cc
aulin <hoque.talha@gmail.com>
2021-04-23 18:45:54 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to domain1.com</title>
+  </head>
+  <body>
+
+  	<?php echo"welcosdfafme"; ?>
+
+
+    <h1>Success! domain1.com tyhertytruryuhome page!</h1>
+  </body>
+</html>
