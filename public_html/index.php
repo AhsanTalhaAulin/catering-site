8f751a3b5794f32b8698203ded81ab5cdf56cd5f
aulin <hoque.talha@gmail.com>
2021-04-26 13:15:28 +0600

    Start anew --build from scratch

---
+++ public_html/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Catering Site</title>
+
+    <?php
+        include "./html/head-essentials.html";
+    ?>
+
+</head>
+
+<body>
+
+    <!-- PRE LOADER -->
+    <?php
+        include "./html/preloader.html";
+    ?>
+
+
+    <!-- MENU -->
+
+	
+    <?php
+        include "./html/header.html";
+    ?>
+
+
+    <!-- HOME -->
+    <?php
+        include "./html/index_element/home.html";
+    ?>
+
+
+    <!-- ABOUT -->
+    <?php
+        include "./html/index_element/about.html";
+    ?>
+
+    <!-- TEAM -->
+    <?php
+        include "./html/index_element/team.html";
+    ?>
+
+
+    <!--POPULAR MENU-->
+    <?php
+        include "./html/index_element/popular-menu.html";
+    ?>
+
+
+
+    <!-- TESTIMONIAL -->
+    <?php
+        include "./html/index_element/testimonial.html";
+    ?>
+
+
+
+    <!-- CONTACT -->
+    <?php
+        include "./html/index_element/contact.html";
+    ?>
+
+
+
+    <!-- FOOTER -->
+    <?php
+        include "./html/footer.html";
+    ?>
+
+
+
+    <!-- SCRIPTS -->
+    <?php
+        include "./html/script-list.html";
+    ?>
+
+</body>
+
+</html>
+
+
